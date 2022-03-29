dfab9c5db949bd5ac37162e0d01a7eaea5d7982b
Outs3 <3.3nosekai@gmail.com>
2022-03-29 10:22:31 +0800

    0.0.61

---
+++ sbhyi-compose/src/main/java/com/outs/core/android/compose/ComposeExt.kt
@@ -0,0 +1,11 @@
+package com.outs.core.android.compose
+
+import androidx.compose.runtime.Composable
+
+/**
+ * author: Outs3
+ * e-mail: 3.3nosekai@gmail.com
+ * date: 2022/3/29 9:23
+ * desc:
+ */
+val emptyComposable: @Composable () -> Unit = {}
