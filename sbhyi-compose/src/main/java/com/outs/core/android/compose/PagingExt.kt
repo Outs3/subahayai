077962657c10b4fa35527951c698685d96848580
Outs3 <3.3nosekai@gmail.com>
2022-04-11 10:41:22 +0800

    0.0.65 Compose RefreshList组件

--- sbhyi-compose/src/main/java/com/outs/core/android/compose/PagingExt.kt
+++ sbhyi-compose/src/main/java/com/outs/core/android/compose/PagingExt.kt
@@ -3,6 +3,7 @@ package com.outs.core.android.compose
 import androidx.compose.foundation.lazy.LazyListState
 import com.google.accompanist.swiperefresh.SwipeRefreshState
 import com.outs.utils.android.IntPagingSource
+import kotlinx.coroutines.flow.MutableStateFlow
 
 /**
  * author: Outs3
@@ -10,15 +11,51 @@ import com.outs.utils.android.IntPagingSource
  * date: 2022/4/8 17:41
  * desc:
  */
-abstract class RefreshIntPagingSource<T : Any>(
-    protected val refreshState: SwipeRefreshState? = null,
-    protected val listState: LazyListState? = null
-) : IntPagingSource<T>() {
+abstract class LoadingIntPagingSource<T : Any> : IntPagingSource<T>() {
+
+    protected val isLoading by lazy { MutableStateFlow(false) }
+
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
-        refreshState?.isRefreshing = true
+        onLoadStart(params)
         val ret = super.load(params)
-        refreshState?.isRefreshing = false
-        listState?.scrollToItem(0)
+        onLoadResult(ret)
         return ret
     }
+
+    open suspend fun onLoadStart(params: LoadParams<Int>) {
+        isLoading.value = true
+    }
+
+    open suspend fun onLoadResult(result: LoadResult<Int, T>) {
+        isLoading.value = false
+    }
+
+}
+
+abstract class RefreshIntPagingSource<T : Any>(
+    protected val refresh: SwipeRefreshState,
+    protected val listState: LazyListState? = null,
+    protected val onRefreshFinish: suspend () -> Unit = {},
+) : LoadingIntPagingSource<T>() {
+
+    override suspend fun onLoadStart(params: LoadParams<Int>) {
+        super.onLoadStart(params)
+        if (null == params.key || 1 == params.key) {
+            refresh.isRefreshing = true
+        }
+    }
+
+    override suspend fun onLoadResult(result: LoadResult<Int, T>) {
+        super.onLoadResult(result)
+        if (refresh.isRefreshing) {
+            refreshFinish()
+        }
+    }
+
+    private suspend fun refreshFinish() {
+        refresh.isRefreshing = false
+        listState?.scrollToItem(0)
+        onRefreshFinish()
+    }
+
 }
