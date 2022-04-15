855fee94ba36fa232b7906bc4f868138b4d58ace
Outs3 <3.3nosekai@gmail.com>
2022-04-15 10:10:10 +0800

    0.0.68 TextField

--- sbhyi-compose/src/main/java/com/outs/core/android/compose/widgets/DecorationTextField.kt
+++ sbhyi-compose/src/main/java/com/outs/core/android/compose/widgets/DecorationTextField.kt
@@ -1,53 +1,54 @@
 package com.outs.core.android.compose.widgets
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.TextFieldColors
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.dp
-import com.outs.core.android.compose.drawIndicatorLine
-import com.outs.core.android.compose.theme.GrayE6
 
 /**
  * author: Outs3
  * e-mail: 3.3nosekai@gmail.com
- * date: 2022/4/2 13:42
+ * date: 2022/4/15 10:03
  * desc:
  */
 @Composable
-fun LineTextField(
+fun DecorationTextField(
+    modifier: Modifier = Modifier,
     value: String,
     onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
     enabled: Boolean = true,
     readOnly: Boolean = false,
-    textStyle: TextStyle = TextStyle.Default,
+    textStyle: TextStyle = LocalTextStyle.current,
     contentStart: @Composable (RowScope.() -> Unit)? = null,
     contentEnd: @Composable (RowScope.() -> Unit)? = null,
     placeholder: @Composable (BoxScope.() -> Unit)? = null,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    singleLine: Boolean = false,
+    maxLines: Int = Int.MAX_VALUE,
     visualTransformation: VisualTransformation = VisualTransformation.None,
     onTextLayout: (TextLayoutResult) -> Unit = {},
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    colors: TextFieldColors = TextFieldDefaults.textFieldColors(
-        unfocusedIndicatorColor = GrayE6
-    )
+    cursorBrush: Brush = SolidColor(Color.Black)
 ) {
     var textFieldValueState by remember { mutableStateOf(TextFieldValue(text = value)) }
     val textFieldValue = textFieldValueState.copy(text = value)
-
-    LineTextField(
+    DecorationTextField(
         value = textFieldValue,
         onValueChange = {
             textFieldValueState = it
@@ -64,59 +65,63 @@ fun LineTextField(
         placeholder = placeholder,
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
+        singleLine = singleLine,
+        maxLines = maxLines,
         visualTransformation = visualTransformation,
         onTextLayout = onTextLayout,
         interactionSource = interactionSource,
-        colors = colors,
+        cursorBrush = cursorBrush,
     )
 }
 
 @Composable
-fun LineTextField(
+fun DecorationTextField(
+    modifier: Modifier = Modifier,
     value: TextFieldValue,
     onValueChange: (TextFieldValue) -> Unit,
-    modifier: Modifier = Modifier,
     enabled: Boolean = true,
     readOnly: Boolean = false,
-    textStyle: TextStyle = TextStyle.Default,
+    textStyle: TextStyle = LocalTextStyle.current,
     contentStart: @Composable (RowScope.() -> Unit)? = null,
     contentEnd: @Composable (RowScope.() -> Unit)? = null,
     placeholder: @Composable (BoxScope.() -> Unit)? = null,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    singleLine: Boolean = false,
+    maxLines: Int = Int.MAX_VALUE,
     visualTransformation: VisualTransformation = VisualTransformation.None,
     onTextLayout: (TextLayoutResult) -> Unit = {},
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    colors: TextFieldColors = TextFieldDefaults.textFieldColors(
-        unfocusedIndicatorColor = GrayE6
-    ),
+    cursorBrush: Brush = SolidColor(Color.Black)
 ) {
-    val indicatorWidth = 1.dp
-    val indicatorColor = colors.indicatorColor(
-        enabled = enabled,
-        isError = false,
-        interactionSource = interactionSource
-    )
-    DecorationTextField(
+    val decorationBox: @Composable (innerTextField: @Composable () -> Unit) -> Unit =
+        { innerTextField ->
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                contentStart?.invoke(this)
+                Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.CenterStart) {
+                    if (value.text.isEmpty()) {
+                        placeholder?.invoke(this)
+                    }
+                    innerTextField()
+                }
+                contentEnd?.invoke(this)
+            }
+        }
+    BasicTextField(
         value = value,
         onValueChange = onValueChange,
-        modifier = modifier.drawIndicatorLine(
-            lineWidth = indicatorWidth,
-            color = indicatorColor.value
-        ),
+        modifier = modifier,
         enabled = enabled,
         readOnly = readOnly,
         textStyle = textStyle,
-        contentStart = contentStart,
-        contentEnd = contentEnd,
-        placeholder = placeholder,
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
-        singleLine = true,
-        maxLines = 1,
+        singleLine = singleLine,
+        maxLines = maxLines,
         visualTransformation = visualTransformation,
         onTextLayout = onTextLayout,
         interactionSource = interactionSource,
-        cursorBrush = SolidColor(colors.cursorColor(isError = false).value),
+        cursorBrush = cursorBrush,
+        decorationBox = decorationBox
     )
 }
