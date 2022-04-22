f1a19296eeedacd670d891144d32c9c0a5bd5ff7
Outs3 <3.3nosekai@gmail.com>
2022-04-22 15:03:25 +0800

    0.0.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-//buildscript {
-//    repositories {
-//        google()
-//        mavenCentral()
-//        importMavens()
-//    }
-//    dependencies {
-//        classpath("com.android.tools.build:gradle:7.1.3")
-//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN_VERSION}")
-//
-//        // NOTE: Do not place your application dependencies here; they belong
-//        // in the individual module build.gradle files
-//    }
-//}
-
 plugins {
-    id("com.android.application").version("7.1.3").apply(false)
-    id("com.android.library").version("7.1.3").apply(false)
-    id("org.jetbrains.kotlin.android").version(Versions.KOTLIN_VERSION).apply(false)
-    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
-//    id("org.jetbrains.kotlin:kotlin-gradle-plugin").version(Versions.KOTLIN_VERSION).apply(false)
+    id("com.android.application") version "7.1.3" apply false
+    id("com.android.library") version "7.1.3" apply false
+    id("org.jetbrains.kotlin.android") version Versions.KOTLIN_VERSION apply false
+    id("org.jetbrains.kotlin.jvm") version Versions.KOTLIN_VERSION apply false
 }
 
 tasks.register("clean", Delete::class) {
