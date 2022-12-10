5df3c5319cf3ccd49d20b8a3d0a6f1098a45208a
T-_3 <3.3nosekai@gmail.com>
2022-12-10 21:17:49 +0800

    0.0.84

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,9 +1,9 @@
 object ConfigData {
-    const val COMPILE_SDK_VERSION = 32
-    const val BUILD_TOOLS_VERSION = "30.0.3"
+    const val COMPILE_SDK_VERSION = 33
+    const val BUILD_TOOLS_VERSION = "33.0.1"
     const val applicationId = "com.outs.subahayai"
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 32
+    const val TARGET_SDK_VERSION = 33
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 
